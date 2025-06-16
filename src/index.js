20dbe3461043b4a8194b23042db685cdeae6e81d
kusumasri-ts <kusumasrits25@gmail.com>
2025-06-16 18:23:23 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const App = () => <h2>FemiBloom Yoga App Running!</h2>;
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
