9e357a3ed94e5444f8e9706387249c81e21981fd
kusumasri-ts <kusumasrits25@gmail.com>
2025-06-16 21:06:47 -0400

    Initial website commit: FemiBloom Yoga

--- src/index.js
+++
@@ -1,8 +0,0 @@
-
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-
-const App = () => <h2>FemiBloom Yoga App Running!</h2>;
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
